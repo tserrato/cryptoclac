998773df87688316e3df8bca0b700ad72018efe8
Tyler Serrato <21351@columbushs.com>
2020-02-25 15:41:50 -0500

    working

--- script.js
+++ script.js
@@ -1,42 +1,65 @@
-// var apikey = {
-//     key:'cfc1cb36-d937-441c-8d9a-5964f940434c'
-// }
-    
-// request('GET','https://pro-api.coinmarketcap.com/v1/cryptocurrency/map?CMC_PRO_API_KEY=' + apikey.key)
-// .then((r1) => {
-//     console.log(r1);
-// }).catch(err => {
-//     console.log(err);
-// })  
-    
-// function request(method, url) {
-//         return new Promise(function (resolve, reject) {
-//             var xhr = new XMLHttpRequest();
-//             xhr.open(method, url);
-//             xhr.onload = resolve;
-//             xhr.onerror = reject;
-//             xhr.send();
-//         });
-// }
- 
-/* Example in Node.js ES6 using request-promise */
+var apikey = {
+    key:'cfc1cb36-d937-441c-8d9a-5964f940434c'
+}
 
-const rp = require('request-promise');
-const requestOptions = {
-  method: 'GET',
-  uri: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/map',
-  qs: {
-    'symbol': 'BTC,USDT,BNB'
-  },
-  headers: {
-    'X-CMC_PRO_API_KEY': 'b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c'
-  },
-  json: true,
-  gzip: true
-};
+function request(method, url) {
+  return new Promise(function (resolve, reject) {
+      var xhr = new XMLHttpRequest();
+      xhr.open(method, url);
+      xhr.onload = resolve;
+      xhr.onerror = reject;
+      xhr.send();
+  });
+}
+    
+function getBTC(){
+  request('GET','https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=12&CMC_PRO_API_KEY=' + apikey.key)
+.then((r1) => {
+  return (JSON.parse(r1.target.response).data[0].quote.USD.price);
+}).catch(err => {
+    console.log(err);
+})
+}  
 
-rp(requestOptions).then(response => {
-  console.log('API call response:', response);
-}).catch((err) => {
-  console.log('API call error:', err.message);
-});
+function getETH(){
+  request('GET','https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=12&CMC_PRO_API_KEY=' + apikey.key)
+.then((r1) => {
+  return (JSON.parse(r1.target.response).data[1].quote.USD.price);
+}).catch(err => {
+    console.log(err);
+})
+}
+
+function getLTC(){
+  request('GET','https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=12&CMC_PRO_API_KEY=' + apikey.key)
+.then((r1) => {
+  return (JSON.parse(r1.target.response).data[6].quote.USD.price);
+}).catch(err => {
+    console.log(err);
+})
+}
+
+function getXMR(){
+  request('GET','https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=12&CMC_PRO_API_KEY=' + apikey.key)
+.then((r1) => {
+  return (JSON.parse(r1.target.response).data[11].quote.USD.price);
+}).catch(err => {
+    console.log(err);
+})
+}  
+    
+
+let prices = [];
+
+
+
+function arrayPush(){
+  prices.push(getBTC());
+  prices.push(getETH());
+  prices.push(getLTC());
+  prices.push(getXMR());
+
+  console.log(prices);
+}
+
+arrayPush();
